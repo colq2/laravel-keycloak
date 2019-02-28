dde3039403a8da0fcd8998b8e501415b6227d1f1
Oliver Wycisk <development@o-wycisk.de>
2019-02-28 16:44:37 +0100

    Added tests

--- tests/Unit/ServiceProviderTest.php
+++ tests/Unit/ServiceProviderTest.php
@@ -2,6 +2,13 @@
 
 namespace colq2\Tests\Keycloak\Unit;
 
+use colq2\Keycloak\Contracts\Authenticator;
+use colq2\Keycloak\Contracts\Gateway;
+use colq2\Keycloak\Contracts\TokenChecker;
+use colq2\Keycloak\Contracts\TokenFinder;
+use colq2\Keycloak\Contracts\TokenStorage;
+use colq2\Keycloak\Contracts\UserService;
+use colq2\Keycloak\KeycloakGuard;
 use colq2\Keycloak\KeycloakProvider;
 use colq2\Tests\Keycloak\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -16,14 +23,60 @@ class ServiceProviderTest extends TestCase
         $this->assertInstanceOf(SocialiteContract::class, socialite());
     }
 
-    public function testProviderExtendsSocialiteWithCorrectParameters()
+    public function testProviderExtendsSocialiteWithKeycloakProvider()
     {
         $provider = socialite()->driver('keycloak');
 
         $this->assertInstanceOf(KeycloakProvider::class, $provider);
+    }
 
-        if($provider instanceof KeycloakProvider);
+    public function testProviderExtendsAuthWithKeycloakGuard()
+    {
+        $guard = auth()->guard('keycloak');
 
+        $this->assertInstanceOf(KeycloakGuard::class, $guard);
+    }
+
+    public function testProviderRegistersUserService()
+    {
+        $this->assertProviderRegisters(UserService::class);
+
+        $userService1 = $this->app->make(UserService::class);
+        $userService2 = $this->app->make(UserService::class);
+
+        self::assertSame($userService1, $userService2);
+    }
+
+    public function testProviderRegistersAuthenticator()
+    {
+        $this->assertProviderRegisters(Authenticator::class);
+    }
+
+    public function testProviderRegistersTokenFinder()
+    {
+        $this->assertProviderRegisters(TokenFinder::class);
+    }
+
+    public function testProviderRegistersTokenStorage()
+    {
+        $this->assertProviderRegisters(TokenStorage::class);
+    }
+
+    public function testProviderRegistersTokenChecker()
+    {
+        $this->assertProviderRegisters(TokenChecker::class);
+    }
+
+    public function testProviderRegistersGateway()
+    {
+        $this->assertProviderRegisters(Gateway::class);
+    }
+
+    protected function assertProviderRegisters($class)
+    {
+        $this->assertInstanceOf($class,
+            $this->app->make($class)
+        );
     }
 
 }
